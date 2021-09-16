8381e566ab9762098a64e29f37eb82c1d3555a70
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2021-09-16 19:24:59 +0530

    Added heading component that renders dynamic semantic heading elements depending on level

---
+++ components/lib/VHeading.vue
@@ -0,0 +1,21 @@
+<template>
+  <component :is="'h' + level"><slot /></component>
+</template>
+
+<script>
+export default {
+  name: 'VHeading',
+  props: {
+    level: {
+      type: Number,
+      default: 1,
+      validator: (value) => value > 0 && value < 6,
+    },
+    color: {
+      type: String,
+      default: 'primary',
+      validator: (value) => ['primary', 'secondary', 'link'].includes(value),
+    },
+  },
+}
+</script>
