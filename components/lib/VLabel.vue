c891b3a1c7e4843df8fda2ade5b82291f5a28f5a
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2021-09-17 19:19:56 +0530

    Added label component

---
+++ components/lib/VLabel.vue
@@ -0,0 +1,25 @@
+<template>
+  <label v-bind="$attrs" class="font-sans color-primary">{{ value }}</label>
+</template>
+
+<script>
+export default {
+  name: 'VLabel',
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+label {
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 1.125rem;
+  line-height: 1.375rem;
+  margin-bottom: 0.625rem;
+}
+</style>
