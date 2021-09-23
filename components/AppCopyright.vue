22cb8ebd88f51d5da0f8b7502cabb8064385b129
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2021-09-23 10:58:48 +0530

    Added copyright section

---
+++ components/AppCopyright.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-text font="sans" size="1.25rem"> 2021 Arcana Technologies Ltd. </v-text>
+  </div>
+</template>
+
+<script>
+import VText from './lib/VText.vue'
+export default {
+  components: { VText },
+}
+</script>
+
+<style lang="postcss" scoped>
+div {
+  padding: 2.5rem 0;
+  text-align: center;
+}
+</style>
