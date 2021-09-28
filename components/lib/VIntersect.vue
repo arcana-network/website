3fa564d17ddf04239026d4eb0852542ca0c159ff
punaifang <punaifang@users.noreply.github.com>
2021-09-28 23:53:16 +0530

    feat: add abstract intersection observer component

---
+++ components/lib/VIntersect.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: 'VIntersect',
+  abstract: true,
+  props: {
+    threshold: {
+      type: [Number, Array],
+      default: 0,
+    },
+  },
+  emits: ['enter', 'leave'],
+  mounted() {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        const firstEntry = entries[0]
+        this.$emit(firstEntry.isIntersecting ? 'enter' : 'leave', [firstEntry])
+      },
+      {
+        threshold: this.threshold,
+      }
+    )
+
+    this.$nextTick(() => {
+      if (
+        !this.$slots.default ||
+        (this.$slots.default && this.$slots.default.length > 1)
+      ) {
+        console.warn('VIntersect must have exactly one child component.')
+      }
+      this.observer.observe(this.$slots.default[0].elm)
+    })
+  },
+  destroyed() {
+    this.observer.disconnect()
+  },
+  render() {
+    return this.$slots.default ? this.$slots.default[0] : null
+  },
+}
+</script>
