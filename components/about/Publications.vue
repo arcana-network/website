daae2ecb477ed6ef5453d8f43acdd6569c175314
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2021-10-01 13:16:33 +0530

    Added publications section

---
+++ components/about/Publications.vue
@@ -0,0 +1,74 @@
+<template>
+  <section>
+    <v-container class="publication-container">
+      <v-stack justify="space-between" align="center">
+        <v-image
+          path="images/publications-illustration.svg"
+          class="publication-illustration"
+        />
+        <v-stack
+          direction="column"
+          gap="2rem"
+          align="start"
+          class="publication-description-container"
+        >
+          <app-section-descriptor
+            title="PUBLICATION"
+            heading="Keep up to date with our network."
+            description="Our codebase will be publicly verifiable with independent audits to validate the security and promise of ARCANA before Mainnet"
+            class="publication-description"
+          />
+          <v-button
+            label="Technical Paper"
+            label-transform="uppercase"
+            class="technical-paper-btn"
+          />
+        </v-stack>
+      </v-stack>
+    </v-container>
+  </section>
+</template>
+
+<script>
+import AppSectionDescriptor from '../AppSectionDescriptor.vue'
+import VButton from '../lib/VButton.vue'
+import VContainer from '../lib/VContainer.vue'
+import VImage from '../lib/VImage.vue'
+import VStack from '../lib/VStack.vue'
+export default {
+  name: 'Publications',
+  components: { VContainer, VStack, VImage, AppSectionDescriptor, VButton },
+}
+</script>
+
+<style scoped>
+@import url('../lib/media-query-helper.css');
+
+section {
+  margin-top: 10rem;
+  background: #080808
+    linear-gradient(
+      180deg,
+      rgba(18, 18, 18, 0.2) 15.02%,
+      rgba(255, 255, 255, 0.2) 47.35%,
+      rgba(32, 32, 32, 0) 100%
+    );
+}
+
+.publication-container {
+  background: #0b0a0a;
+}
+
+.publication-description-container {
+  width: 100%;
+  max-width: 40rem;
+}
+
+.publication-description {
+  max-width: 24rem;
+}
+
+.technical-paper-btn {
+  padding: 1rem 2rem;
+}
+</style>
--- pages/tech.vue
+++ pages/tech.vue
@@ -2,14 +2,16 @@
   <main>
     <AppHeader />
     <Technology />
+    <Publications />
   </main>
 </template>
 
 <script>
+import Publications from '~/components/about/Publications.vue'
 import Technology from '~/components/about/Technology.vue'
 import AppHeader from '~/components/AppHeader.vue'
 export default {
-  components: { AppHeader, Technology },
+  components: { AppHeader, Technology, Publications },
   head() {
     return {
       title: 'Technology - Arcana Network',
