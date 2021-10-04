689cce4efaa299cd15b89d1b94af34804b87c16b
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2021-10-04 21:34:58 +0530

    Removed manual import

--- pages/tech.vue
+++ pages/tech.vue
@@ -2,16 +2,12 @@
   <main>
     <AppHeader />
     <Technology />
-    <JoinUs />
+    <AboutJoinUs />
   </main>
 </template>
 
 <script>
-import JoinUs from '~/components/about/JoinUs.vue'
-import Technology from '~/components/about/Technology.vue'
-import AppHeader from '~/components/AppHeader.vue'
 export default {
-  components: { AppHeader, Technology, JoinUs },
   head() {
     return {
       title: 'Technology - Arcana Network',
