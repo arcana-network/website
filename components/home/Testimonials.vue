727b8ffd0e200546a5afcee7c5919a8339fb4562
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2021-10-04 12:36:10 +0530

    fix: fixed links for testimonial assets

--- components/home/Testimonials.vue
+++ components/home/Testimonials.vue
@@ -64,21 +64,21 @@ export default {
             'We’ve always been intensely focused on enabling our partners and customers to improve business outcomes. The addition of Arcana’s Security and Privacy will complement our mission of decentralizing our network',
           quotedBy: 'Ram Sharma',
           designation: 'CEO and Co-Founder',
-          companyLogo: '/images/testimonials/unmarshal.svg',
+          companyLogo: 'images/testimonials/unmarshal.svg',
         },
         {
           quote:
             'We are very excited to start working with Arcana Network to improve the developer experience of the crowd building the DIA ecosystem by offering a number of privacy and security features to our tech stack.',
           quotedBy: 'Dia',
           designation: 'CEO and Co-Founder',
-          companyLogo: '/images/testimonials/unmarshal.svg',
+          companyLogo: 'images/testimonials/unmarshal.svg',
         },
         {
           quote:
             'Partnerships are all about pushing the boundaries on innovating for the customer through our products. Our partnership with Arcana will help us deliver exactly that while we explore new and innovative ways to deploy and bring robust scalability and privacy to our technology stack and eventually the RageFan users and community.',
           quotedBy: 'Rage.fan',
           designation: 'CEO and Co-Founder',
-          companyLogo: '/images/testimonials/unmarshal.svg',
+          companyLogo: 'images/testimonials/unmarshal.svg',
         },
       ],
     }
