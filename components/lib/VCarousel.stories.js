a337bb249e941ed30e1c8c7edaa7adb60c0d7379
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2021-10-05 17:36:15 +0530

    Updated vcarousel story

--- components/lib/VCarousel.stories.js
+++ components/lib/VCarousel.stories.js
@@ -1,15 +1,9 @@
 export const carousel = () => `
   <v-container>
     <v-carousel style="height: 10rem">
-        <v-stack align="center" justify="center" style="width: 100%">
-            <v-text>Slide 1</v-text>
-        </v-stack>
-       <v-stack align="center" justify="center" style="width: 100%">
-            <v-heading :level="2">Slide 2</v-heading>
-        </v-stack>
-        <v-stack align="center" justify="center" style="width: 100%">
-            <v-text :weight="600">Slide 3</v-text>
-        </v-stack>
+        <v-text>Slide 1</v-text>
+        <v-heading :level="2">Slide 2</v-heading>
+        <v-text :weight="600">Slide 3</v-text>
     </v-carousel>
   </v-container>
 `
