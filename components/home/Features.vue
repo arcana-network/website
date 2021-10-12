6908f6d044a8829d0550abcd07f48f3e505e90c8
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2021-10-12 14:15:12 +0530

    Reduved testimonials height and removed overflow-x from features

--- components/home/Features.vue
+++ components/home/Features.vue
@@ -17,12 +17,12 @@
         />
       </v-stack>
       <v-stack justify="space-between" md-direction="column" md-align="center">
-        <v-stack
-          direction="column"
-          sm-direction="row"
-          class="features-description-container"
-        >
-          <v-stack justify="space-between" style="margin-top: 2rem">
+        <v-stack direction="column" class="features-description-container">
+          <v-stack
+            sm-direction="column"
+            justify="space-between"
+            class="blurb-container"
+          >
             <v-blurb
               title="Decentralised Storage"
               description="Any assets uploaded to Arcana is encrypted, split up and distributed among multiple storage nodes. Without a single-point of failure your assets are protected against geo-specific disasters and hacking attempts."
@@ -36,14 +36,17 @@
               title="End to End Encryption"
               description="Want to make sure nobody snoops on your users’ data? Implement end-to-end encryption simply and easily with Arcana’s E2E Encryption."
               class="blurb"
-              style="margin-left: 2rem"
             >
               <template #art>
                 <art-encryption-icon animate-on-enter animate-on-hover />
               </template>
             </v-blurb>
           </v-stack>
-          <v-stack justify="space-between" style="margin-top: 2rem">
+          <v-stack
+            sm-direction="column"
+            justify="space-between"
+            class="blurb-container"
+          >
             <v-blurb
               title="Identity and Access Management"
               description="Decentralised Identity and Access Controls are verifiable in a trust-less way and adhere to the W3C DID spec. You can rest easy knowing that your app will only allow verified users to access assets stored on the system."
@@ -57,7 +60,6 @@
               title="Simple Keys Management"
               description="A big hurdle to cryptographic encryption has to do with public+private keys management. We’ve made it simple by generating keys using authentications like email/password and popular Single Sign On’s (SSO’s)."
               class="blurb"
-              style="margin-left: 2rem"
             >
               <template #art>
                 <art-keys-icon animate-on-enter animate-on-hover />
@@ -118,17 +120,24 @@ section {
 @media (--viewport-small) {
   .features-description-container {
     width: 100%;
-    overflow-x: auto;
-    overflow-y: hidden;
   }
+}
 
-  .features-description-container > * + * {
-    margin-left: 2rem;
+.blurb-container {
+  margin-top: 2rem;
+}
+
+.blurb-container > * + * {
+  margin-left: 2rem;
+
+  @media (--viewport-small) {
+    margin-left: 0;
+    margin-top: 2rem;
   }
 }
 
 .blurb {
-  width: 24rem;
+  max-width: 24rem;
 }
 
 .blurb img {
