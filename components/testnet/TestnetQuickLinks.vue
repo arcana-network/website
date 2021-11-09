9903dc34dc2c1fde8e888b8c2169d93042841bb9
punaifang <punaifang@users.noreply.github.com>
2021-11-09 13:43:49 +0530

    Add a submit bugs button

--- components/testnet/TestnetQuickLinks.vue
+++ components/testnet/TestnetQuickLinks.vue
@@ -1,53 +1,70 @@
 <template>
-  <div class="quicklinks">
-    <v-card
-      v-for="(link, linkIndex) in quickLinks"
-      :key="link.name"
-      class="quicklinks-card"
-    >
-      <v-stack
-        align="start"
-        sm-align="center"
-        direction="row"
-        sm-direction="row"
-        justify="space-between"
+  <v-stack
+    direction="column"
+    gap="1.875rem"
+    sm-direction="column"
+    sm-gap="2.5rem"
+  >
+    <div class="quicklinks-cards">
+      <v-card
+        v-for="(link, linkIndex) in quickLinks"
+        :key="link.name"
+        class="quicklinks-card"
       >
         <v-stack
-          direction="column"
-          sm-direction="row"
           align="start"
           sm-align="center"
-          gap="1.25rem"
-          sm-gap="0"
+          direction="row"
+          sm-direction="row"
+          justify="space-between"
         >
-          <div class="position-relative">
-            <v-image class="quicklinks-crater" path="images/crater.svg" />
-            <v-text
-              font="serif"
-              :weight="700"
-              line-height="1.2"
-              class="quicklinks-count"
-            >
-              {{ linkIndex + 1 }}
-            </v-text>
-          </div>
-          <v-text
-            class="quicklinks-description"
-            :weight="400"
-            line-height="1.5"
+          <v-stack
+            direction="column"
+            sm-direction="row"
+            align="start"
+            sm-align="center"
+            gap="1.25rem"
+            sm-gap="0"
           >
-            {{ link.description }}
-          </v-text>
+            <div class="position-relative">
+              <v-image class="quicklinks-crater" path="images/crater.svg" />
+              <v-text
+                font="serif"
+                :weight="700"
+                line-height="1.2"
+                class="quicklinks-count"
+              >
+                {{ linkIndex + 1 }}
+              </v-text>
+            </div>
+            <v-text
+              class="quicklinks-description"
+              :weight="400"
+              line-height="1.5"
+            >
+              {{ link.description }}
+            </v-text>
+          </v-stack>
+          <v-image class="quicklinks-arrow" path="images/arrow-right.svg" />
         </v-stack>
-        <v-image class="quicklinks-arrow" path="images/arrow-right.svg" />
-      </v-stack>
-    </v-card>
-  </div>
+      </v-card>
+    </div>
+    <v-button
+      class="btn-submit-bugs"
+      label="Submit Bugs →"
+      type="button"
+      label-transform="uppercase"
+      label-size="1rem"
+      :label-weight="600"
+    />
+  </v-stack>
 </template>
 
 <script>
+import VButton from '../lib/VButton.vue'
 export default {
   name: 'TestnetQuickLinks',
+  components: { VButton },
   data() {
     return {
       quickLinks: [
@@ -64,7 +81,7 @@ export default {
 <style lang="postcss" scoped>
 @import url('../lib/media-query-helper.css');
 
-.quicklinks {
+.quicklinks-cards {
   display: grid;
   grid-gap: 1.875rem;
   grid-template-columns: repeat(4, 1fr);
@@ -110,6 +127,19 @@ export default {
 .quicklinks-arrow {
   @media (--viewport-small) {
     padding: 1rem;
+  }
+}
+
+.btn-submit-bugs {
+  align-self: end;
+  flex-grow: 0;
+  flex-shrink: 0;
+  width: 16rem;
+
+  @media (--viewport-small) {
+    flex-grow: 1;
+    flex-shrink: 1;
+    width: 100%;
   }
 }
 </style>
