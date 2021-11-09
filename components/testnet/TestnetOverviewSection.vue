3e3e4e56805fa4e6ed5ebdc1c5915b7c27ef0738
punaifang <punaifang@users.noreply.github.com>
2021-11-09 15:33:40 +0530

    Spacing and typography tweaks

--- components/testnet/TestnetOverviewSection.vue
+++ components/testnet/TestnetOverviewSection.vue
@@ -1,139 +1,136 @@
 <template>
   <section>
     <v-container>
-      <v-stack
-        align="start"
-        direction="column"
-        sm-direction="column"
-        gap="30px"
-        sm-gap="60px"
-      >
-        <v-stack
-          align="start"
-          direction="column"
-          sm-direction="column"
-          gap="10px"
-          sm-gap="20px"
-        >
-          <v-heading
-            class="overview-heading"
-            :level="2"
-            line-height="1.2"
-            :weight="700"
+      <v-stack align="start" direction="column" gap="3.75rem">
+        <div>
+          <v-stack
+            align="start"
+            direction="column"
+            sm-direction="column"
+            gap="1.25rem"
+            sm-gap="1.25rem"
           >
-            Overview
-          </v-heading>
-          <v-text
-            class="overview-description"
-            color="secondary"
-            :weight="400"
-            line-height="1.5"
+            <v-heading
+              class="overview-heading"
+              :level="2"
+              line-height="1.2"
+              :weight="700"
+            >
+              Overview
+            </v-heading>
+            <v-text
+              class="overview-description"
+              color="secondary"
+              :weight="400"
+              line-height="1.5"
+            >
+              Announcing the Arcana Testnet: Explore usecases and hunt bugs.
+              This is our Alpha testnet to give developers access to our SDK,
+              explore new usecases, and help improve Arcana Network ahead of
+              mainnet launch.
+            </v-text>
+            <v-text
+              class="overview-description"
+              color="secondary"
+              :weight="400"
+              line-height="1.5"
+            >
+              Once testnet ends, new developer signups will be put on a waitlist
+              until the next Testnet.
+            </v-text>
+          </v-stack>
+        </div>
+        <div>
+          <v-stack
+            align="start"
+            direction="column"
+            sm-direction="column"
+            gap="1.875rem"
+            sm-gap="1.25rem"
           >
-            Announcing the Arcana Testnet: Explore usecases and hunt bugs. This
-            is our Alpha testnet to give developers access to our SDK, explore
-            new usecases, and help improve Arcana Network ahead of mainnet
-            launch.
-          </v-text>
-          <v-text
-            class="overview-description"
-            color="secondary"
-            :weight="400"
-            line-height="1.5"
-          >
-            Once testnet ends, new developer signups will be put on a waitlist
-            until the next Testnet.
-          </v-text>
-        </v-stack>
-        <v-stack
-          align="start"
-          direction="column"
-          sm-direction="column"
-          gap="30px"
-          sm-gap="20px"
-        >
-          <v-heading
-            class="overview-features-heading"
-            :level="3"
-            line-height="1.2"
-            :weight="600"
-          >
-            Features of Testnet
-          </v-heading>
-          <v-tabs :tabs-list="tabsList">
-            <template #tab-0>
-              <v-stack direction="column" gap="3.75rem">
-                <v-text
-                  class="feature-overview-description"
-                  color="secondary"
-                  :weight="400"
-                  line-height="1.5"
-                >
-                  This testnet will solely focus on the SDK and its core
-                  functionality.
-                </v-text>
-                <v-stack justify="space-between" wrap style="width: 80vw">
-                  <div
-                    v-for="featureOverviewPoint in featureOverviewPoints"
-                    :key="featureOverviewPoint.title"
-                    class="feature-overview-points"
+            <v-heading
+              class="overview-features-heading"
+              :level="3"
+              line-height="1.2"
+              :weight="600"
+            >
+              Features of Testnet
+            </v-heading>
+            <v-tabs :tabs-list="tabsList">
+              <template #tab-0>
+                <v-stack direction="column" gap="3.75rem">
+                  <v-text
+                    class="feature-overview-description"
+                    color="secondary"
+                    :weight="400"
+                    line-height="1.5"
                   >
-                    <v-blurb
-                      :image="featureOverviewPoint.icon"
-                      :alt="featureOverviewPoint.title"
-                      :title="featureOverviewPoint.title"
-                      :description="featureOverviewPoint.description"
-                    />
+                    This testnet will solely focus on the SDK and its core
+                    functionality.
+                  </v-text>
+                  <v-stack justify="space-between" wrap style="width: 80vw">
+                    <div
+                      v-for="featureOverviewPoint in featureOverviewPoints"
+                      :key="featureOverviewPoint.title"
+                      class="feature-overview-points"
+                    >
+                      <v-blurb
+                        :image="featureOverviewPoint.icon"
+                        :alt="featureOverviewPoint.title"
+                        :title="featureOverviewPoint.title"
+                        :description="featureOverviewPoint.description"
+                      />
+                    </div>
+                  </v-stack>
+                </v-stack>
+              </template>
+              <template #tab-1>
+                <v-stack
+                  align="start"
+                  sm-align="start"
+                  direction="row"
+                  sm-direction="column"
+                  justify="start"
+                  sm-justify="baseline"
+                  gap="3.75rem"
+                  sm-gap="3.75rem"
+                  class="feature-capabilities"
+                >
+                  <v-chip class="feature-capabilities-dos">
+                    <v-stack direction="column" gap="2.25rem">
+                      <v-heading
+                        class="feature-capabilities-heading"
+                        :level="3"
+                        line-height="1.2"
+                      >
+                        Do&rsquo;s
+                      </v-heading>
+                      <v-list
+                        :list-items="featureCapabilitiesDoItems"
+                        variant="positive"
+                      />
+                    </v-stack>
+                  </v-chip>
+                  <div class="feature-capabilities-donts">
+                    <v-stack direction="column" gap="2.25rem">
+                      <v-heading
+                        class="feature-capabilities-heading"
+                        :level="3"
+                        line-height="1.2"
+                      >
+                        Dont&rsquo;s
+                      </v-heading>
+                      <v-list
+                        :list-items="featureCapabilitiesDontItems"
+                        variant="negative"
+                      />
+                    </v-stack>
                   </div>
                 </v-stack>
-              </v-stack>
-            </template>
-            <template #tab-1>
-              <v-stack
-                align="start"
-                sm-align="start"
-                direction="row"
-                sm-direction="column"
-                justify="start"
-                sm-justify="baseline"
-                gap="3.75rem"
-                sm-gap="3.75rem"
-                class="feature-capabilities"
-              >
-                <v-chip class="feature-capabilities-dos">
-                  <v-stack direction="column" gap="2.25rem">
-                    <v-heading
-                      class="feature-capabilities-heading"
-                      :level="3"
-                      line-height="1.2"
-                    >
-                      Do&rsquo;s
-                    </v-heading>
-                    <v-list
-                      :list-items="featureCapabilitiesDoItems"
-                      variant="positive"
-                    />
-                  </v-stack>
-                </v-chip>
-                <div class="feature-capabilities-donts">
-                  <v-stack direction="column" gap="2.25rem">
-                    <v-heading
-                      class="feature-capabilities-heading"
-                      :level="3"
-                      line-height="1.2"
-                    >
-                      Dont&rsquo;s
-                    </v-heading>
-                    <v-list
-                      :list-items="featureCapabilitiesDontItems"
-                      variant="negative"
-                    />
-                  </v-stack>
-                </div>
-              </v-stack>
-            </template>
-          </v-tabs>
-        </v-stack>
-
+              </template>
+            </v-tabs>
+          </v-stack>
+        </div>
         <div class="feature-overview-illustration full-bleed" />
       </v-stack>
     </v-container>
@@ -208,15 +205,15 @@ export default {
 .overview-heading {
   margin-top: 0;
   margin-bottom: 0;
-  font-size: 4rem;
+  font-size: 3.125rem;
 
   @media (--viewport-small) {
-    font-size: 2.25rem;
+    font-size: 2.1875rem;
   }
 }
 
 .overview-description {
-  max-width: 42ch;
+  max-width: 46ch;
   font-size: 1.375rem;
 
   @media (--viewport-small) {
@@ -237,6 +234,7 @@ export default {
 
 .feature-overview-description {
   font-size: 1.375rem;
+  max-width: 46ch;
 
   @media (--viewport-small) {
     font-size: 1rem;
