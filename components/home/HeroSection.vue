e1126680a32b106317e4f4b21bbcb4c24e1804b9
Ajith Ranka <ajithranka@users.noreply.github.com>
2021-11-16 15:10:07 +0530

    Add a callout to the testnet launch page

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -6,32 +6,45 @@
           <v-heading :level="1" style="max-width: 38rem">
             The Storage and Privacy Stack for Ethereum dApps
           </v-heading>
-          <app-section-descriptor
-            description="Decentralised storage, identity, access management and key
+          <v-stack direction="column" gap="2rem">
+            <app-section-descriptor
+              description="Decentralised storage, identity, access management and key
             management services to help developers build secure and
             privacy-preserving apps."
-            style="max-width: 38rem"
-          />
-          <v-stack
-            md-direction="column"
-            align="center"
-            gap="1.5rem"
-            md-gap="1.5rem"
-            style="margin-top: 2rem"
-          >
-            <v-button
-              label="Get started"
-              label-transform="uppercase"
-              :action="onGetStarted"
+              style="max-width: 38rem"
             />
-            <v-stack align="center" class="support-icons">
-              <a href="https://t.me/ArcanaNetwork" target="_blank">
-                <v-image path="images/telegram.svg" :dimension="[36, 36]" />
-              </a>
-              <a href="https://twitter.com/arcananetwork" target="_blank">
-                <v-image path="images/twitter.png" :dimension="[36, 36]" />
-              </a>
+            <v-stack
+              md-direction="column"
+              align="center"
+              gap="1.5rem"
+              md-gap="1.5rem"
+            >
+              <v-button
+                label="Get started"
+                label-transform="uppercase"
+                :action="onGetStarted"
+              />
+              <v-stack align="center" class="support-icons">
+                <a href="https://t.me/ArcanaNetwork" target="_blank">
+                  <v-image path="images/telegram.svg" :dimension="[36, 36]" />
+                </a>
+                <a href="https://twitter.com/arcananetwork" target="_blank">
+                  <v-image path="images/twitter.png" :dimension="[36, 36]" />
+                </a>
+              </v-stack>
             </v-stack>
+            <v-chip class="testnet-chip">
+              <v-text size="1.25rem" line-height="1.4" color="secondary">
+                Participate in Arcana's Alpha Testnet Launch!
+                <a
+                  class="testnet-chip-link"
+                  href="http://testnet.arcana.network"
+                  target="_blank"
+                >
+                  See details &rarr;
+                </a>
+              </v-text>
+            </v-chip>
           </v-stack>
         </v-stack>
         <div class="section-orbs">
@@ -87,5 +100,25 @@ section {
   @media (--viewport-medium) {
     margin-left: 1.5rem;
   }
+}
+
+.testnet-chip {
+  max-width: unset;
+  padding: 1.5rem;
+
+  @media (--viewport-small) {
+    width: calc(100% - 3rem);
+  }
+}
+
+.testnet-chip-link {
+  color: var(--color-secondary);
+  text-decoration: none;
+  opacity: 0.7;
+  transition: opacity 0.2s ease-in;
+}
+
+.testnet-chip-link:hover {
+  opacity: 1;
 }
 </style>
