326cc80ae77914c2d43969d904ea2991d5855f6c
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-02-08 18:16:03 +0530

    remove description, align elements as per design

--- components/PageNotFound.vue
+++ components/PageNotFound.vue
@@ -8,18 +8,13 @@
         md-justify="space-evenly"
         class="error-content-container"
       >
-        <v-stack direction="column" class="error-content">
-          <v-heading>Page Not Found!</v-heading>
-          <v-text color="secondary" size="1.25rem" line-height="1.75rem">
-            Decentralised storage, identity, access management and key
-            management services to help developers build secure and
-            privacy-preserving apps
-          </v-text>
-        </v-stack>
         <v-image
           path="images/404-illustration.png"
           class="error-illustration"
         />
+        <v-stack direction="column" class="error-content">
+          <v-heading>Page Not Found!</v-heading>
+        </v-stack>
       </v-stack>
     </v-container>
   </section>
@@ -28,6 +23,8 @@
 <style lang="postcss" scoped>
 .error-content-container {
   height: 80vh;
+  display: flex;
+  flex-direction: column;
 }
 
 .error-content {
