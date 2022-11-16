84be4f2a323a74426d2f7a72bb26a617097644a0
karthikdurai <iamkarthiksiddarth@gmail.com>
2022-11-16 16:28:58 +0530

    create header and footer component for mainnet

---
+++ components/MainnetFooter.vue
@@ -0,0 +1,82 @@
+<template>
+  <footer>
+    <v-stack class="footer-social-link-container">
+      <a href="https://twitter.com/arcananetwork" target="_blank">
+        <v-image
+          style="height: 25px"
+          path="images/twitter-white.svg"
+          class="footer-social-link"
+          alt="Arcana twitter page"
+        />
+      </a>
+      <a href="https://t.me/ArcanaNetwork" target="_blank">
+        <v-image
+          style="height: 25px"
+          path="images/telegram-white.svg"
+          class="footer-social-link"
+          alt="Arcana telegram channel"
+        />
+      </a>
+      <a :href="ARCANA_DISCORD_URL" target="_blank">
+        <v-image
+          style="height: 25px"
+          path="images/discord-white.svg"
+          class="footer-social-link"
+          alt="Arcana discord channel"
+        />
+      </a>
+      <a href="https://forum.arcana.network" target="_blank">
+        <v-image
+          style="height: 25px"
+          path="images/discourse-white.svg"
+          class="footer-social-link"
+          alt="Arcana discourse channel"
+        />
+      </a>
+      <a href="https://www.youtube.com/c/ArcanaNetwork" target="_blank">
+        <v-image
+          style="height: 25px"
+          path="images/youtube-white.svg"
+          class="footer-social-link"
+          alt="Arcana Youtube channel"
+        />
+      </a>
+    </v-stack>
+  </footer>
+</template>
+
+<script>
+const ARCANA_DISCORD_URL = process.env.ARCANA_DISCORD_URL
+
+export default {
+  name: 'AppFooter',
+  data() {
+    return {
+      ARCANA_DISCORD_URL,
+    }
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+@import url('./lib/media-query-helper.css');
+
+footer {
+  background-size: cover;
+  overflow-x: hidden;
+  padding-bottom: 40px;
+}
+
+.footer-social-link-container {
+  justify-content: center;
+  align-items: baseline;
+}
+
+.footer-social-link-container * + * {
+  margin-left: 35px;
+}
+
+.footer-social-link {
+  width: 3rem;
+}
+</style>
---
+++ components/MainnetHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <header class="header">
+    <v-header
+      logo="images/arcana-logo-vertical.svg"
+      :cta="headerCta"
+      :menu-items="menuItems"
+    />
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  data() {
+    return {
+      headerCta: {
+        label: 'Start Building',
+        link: process.env.ARCANA_DASHBOARD_URL,
+      },
+      menuItems: [
+        {
+          name: 'Docs',
+          link: process.env.ARCANA_DOCS_URL,
+          external: true,
+        },
+        {
+          name: 'Technology',
+          link: '/tech/',
+        },
+        {
+          name: 'About',
+          link: '/about/',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.header {
+  position: sticky;
+  top: 0;
+  z-index: 100;
+  background-color: transparent;
+}
+</style>
